ef889a32ee4b51c0191780603d2bf32309355422
khulrtz <jvaldiviaparra@gmail.com>
2021-09-18 12:47:47 +0200

    ATM

---
+++ snapshots/T_C1_SAT_REGION.sql
@@ -0,0 +1,19 @@
+{% snapshot T_C1_SAT_REGION%}
+{{
+config(
+target_database='SDG_DV_TEST',
+target_schema='SDG_DV_STG',
+unique_key='ID_REGION',
+
+strategy='timestamp',
+updated_at='TS_LOADED_AT',
+)
+}}
+
+SELECT HASH(R_REGIONKEY) AS ID_REGION,
+R_NAME AS ST_REG_NAME,
+R_COMMENT AS LT_REG_COMMENT,
+'SDG_DV_TEST_INGEST.REGION' AS ST_SOURCE_FROM,
+CURRENT_TIMESTAMP AS TS_LOADED_AT
+FROM SDG_DV_TEST_INGEST.REGION
+{% endsnapshot %}
